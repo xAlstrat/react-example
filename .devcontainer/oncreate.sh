978c4528394214eb6d4c5fc6bea1a5055b575fa0
Ednar Alejandro Echeverría Carrasco <rodak.ro@gmail.com>
2024-10-01 04:17:28 +0000

    update oncreate

--- .devcontainer/oncreate.sh
+++ .devcontainer/oncreate.sh
@@ -9,6 +9,8 @@ cat .devcontainer/bashrc_epilog.sh >> ~/.bashrc
 # Env
 cp .env.example .env
 
+git config --global --add safe.directory /workspaces/react-example
+
 # Install nodejs
 # curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - &&\
 # sudo apt-get install -y nodejs
