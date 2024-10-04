0437171d9bd11fb851a43a6df4f424b224ddcc31
Ednar Alejandro Echeverría Carrasco <rodak.ro@gmail.com>
2024-10-04 19:34:22 +0000

    update dev container

--- .devcontainer/oncreate.sh
+++ .devcontainer/oncreate.sh
@@ -6,11 +6,35 @@ set -x
 # Run from repo root
 cat .devcontainer/bashrc_epilog.sh >> ~/.bashrc
 
-# Env
-cp .env.example .env
-
 git config --global --add safe.directory /workspaces/react-example
+
+pip install --no-cache git+https://gitlab.com/codematos/pluscoder.git@develop
 
 # Install nodejs
 # curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - &&\
 # sudo apt-get install -y nodejs
+
+# Install python dependencies
+# apt-get update && apt-get install -y \
+#     wget \
+#     build-essential \
+#     zlib1g-dev \
+#     libncurses5-dev \
+#     libgdbm-dev \
+#     libnss3-dev \
+#     libssl-dev \
+#     libsqlite3-dev \
+#     libreadline-dev \
+#     libffi-dev \
+#     curl \
+#     libbz2-dev
+
+# # Install python
+# wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0a7.tgz \
+#     && tar xzf Python-3.12.0a7.tgz \
+#     && cd Python-3.12.0a7 \
+#     && ./configure --enable-optimizations \
+#     && make altinstall \
+#     && cd .. \
+#     && rm -rf Python-3.12.0a7 Python-3.12.0a7.tgz
+
