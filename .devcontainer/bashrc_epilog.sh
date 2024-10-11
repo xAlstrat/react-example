baf89b1791be5933b98580d98159ea8521a60818
Ednar Echeverria <ednarecheverria.neuralworks@latam.com>
2024-10-11 13:11:07 -0300

    update

--- .devcontainer/bashrc_epilog.sh
+++ .devcontainer/bashrc_epilog.sh
@@ -1,3 +1,4 @@
+export ANTHROPIC_API_KEY=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
 echo "To run Pluscoder, in the terminal: plus-coder"
 echo "Show repository context: plus-coder --show_repo"
 echo "Check README.md to configure credentials. Check https://gitlab.com/codematos/pluscoder-repository/-/blob/main/README.md for more docs."
