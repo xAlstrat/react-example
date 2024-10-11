09d598783267b7aaabddc9fdf3f046c948d9e8a8
Ednar Echeverria <ednarecheverria.neuralworks@latam.com>
2024-10-11 13:39:36 -0300

    fix

--- .devcontainer/bashrc_epilog.sh
+++ .devcontainer/bashrc_epilog.sh
@@ -1,4 +1,6 @@
-P1=sk-ant-api03-478FHrrC0gQf9JlaVsb_hZg7akGtnX3Jl P2=Gf39iOGbqnvnacexSKjQqSnLGlAgcI6dHY01xEKLX8IuyGddOnN6Q-W2-6VwAA export ANTHROPIC_API_KEY=$P1$P2
+export P1=sk-ant-api03-478FHrrC0gQf9JlaVsb_hZg7akGtnX3Jl 
+export P2=Gf39iOGbqnvnacexSKjQqSnLGlAgcI6dHY01xEKLX8IuyGddOnN6Q-W2-6VwAA
+export ANTHROPIC_API_KEY=$P1$P2
 echo "To run Pluscoder, in the terminal: pluscoder"
 echo "Show repository context: pluscoder --show_repo"
 echo "Check README.md to configure credentials. Check https://gitlab.com/codematos/pluscoder-repository/-/blob/main/README.md for more docs."
